d751a47c1ba82247db9377bfdeb67ccbeb0f688b
Henrique <henriquezambon.s@gmail.com>
2023-12-04 22:28:39 -0300

    fix: updates initials

---
+++ front-end/src/styles/theme.js
@@ -0,0 +1,27 @@
+export default {
+  COLORS: {
+    BACKGROUND_900: "#000A0F", /*Cor de fundo geral da página*/
+    BACKGROUND_800: "#0D1D25", /*Cor de fundo dos inputs*/
+    BACKGROUND_700: "#92000E", /*Cor de fundo dos botões*/
+    BACKGROUND_600: "hsla(200, 100%, 5%, 1)", /*Cor de fundo do Header*/
+
+    WHITE: "#FFFFFF", /*Cor do texto branco puro*/
+    WHITE_100: "#E1E1E6", /*Cor do texto dos Títulos*/
+
+    BLUE: "#0099ff", /*Cor do logo e dos textos rosas/azuis*/
+    BLUE_100: "#82F3FF", /*Cor dos preços nos produtos*/
+    BLUE_200: "#001119", /*Ccor de fundo do form de login*/
+    BLUE_300: "#192227", /*Cor das bordas das tabelas*/
+    BLUE_400: "#091E26", /* 1ª cor de fundo do gradiente na Home*/
+    BLUE_500: "#00131C", /* 2ª cor de fundo do gradiente na Home*/
+
+    GRAY_100: "#C4C4CC", /* or do texto das descrições*/
+    GRAY_200: "rgba(255, 255, 255, 0.3)", /*Cor do texto do Footer*/
+    GRAY_300: "#7C7C8A", /*Cor do texto nos Inputs*/
+    GRAY_400: "rgba(255, 255, 255, 0.05)", /*Cor dos botões de Pix e Cartão*/
+
+    RED: "#FF002E", /*Cor do ícone para apagar marcadores*/
+    BLACK: "#0D0C0F", /*Cor preta*/
+    FULL_BLACK: "#000000" /*Cor 100% preta*/
+  }
+}
